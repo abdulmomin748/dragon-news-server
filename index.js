ee7b733c768f055023a5a3f2ffac7c38dc8bce62
abdul momin <mominurrahman10748@gmail.com>
2022-10-20 21:28:12 +0600

    added dragon server

---
+++ index.js
@@ -0,0 +1,21 @@
+// entry point
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT  || 5000;
+
+app.use(cors());
+const categories = require('./data/categories.json') // get categories from local folder
+
+app.get('/', (req, res) => {
+    res.send('Welcome to express server!')
+
+});
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories);  // Send categories  
+});
+
+app.listen(port, () => {
+    console.log('Dragon server running on port ', port);
+})
