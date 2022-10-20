173845ff614f48cec434cfedcce266be01e5cfcf
abdul momin <mominurrahman10748@gmail.com>
2022-10-20 22:24:18 +0600

    push

--- index.js
+++ index.js
@@ -7,15 +7,27 @@ const port = process.env.PORT  || 5000;
 app.use(cors());
 const categories = require('./data/categories.json') // get categories from local folder
 
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Welcome to express server!')
 
 });
 
 app.get('/news-categories', (req, res) => {
-    res.send(categories);  // Send categories  
+    res.send(categories);  // Send categories  j
 });
 
 app.listen(port, () => {
     console.log('Dragon server running on port ', port);
+
+
+
+
+
+
+
 })
